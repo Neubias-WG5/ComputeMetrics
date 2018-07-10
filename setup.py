edc67f9766fd4efa7e8756f5a23b01536aefa595
Romain Mormont <romainmormont@hotmail.com>
2018-07-10 17:02:07 +0200

    update setup files

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@
 
 from setuptools import setup
 
+import neubiaswg5
+
 setup(
     name='Neubias-WG5 Metrics Compute',
-    version='1.0',
-    description='',
+    version=neubiaswg5.__version__,
+    description='Metric computation module of Neubias-WG5',
     packages=['neubiaswg5', 'neubiaswg5.metrics'],
-    url='',
-    install_requires=['tifffile',
-                      'scikit-image',
-                      'scikit-learn'],
-    license='LICENSE',
+    install_requires=['tifffile', 'scikit-image', 'scikit-learn']
 )
